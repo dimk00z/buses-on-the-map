ec28097b7180a287fb7a72f9ea59f2ad410ed50a
Dimk00z <dimk00z@gmail.com>
2021-08-04 11:50:49 +0300

    add bus 156 route

--- server.py
+++ server.py
@@ -2,24 +2,33 @@ import trio
 import json
 from trio_websocket import serve_websocket, ConnectionClosed
 
+from itertools import cycle
+
+
+def load_bus_route(bus_number):
+    with open(f'routes/{bus_number}.json') as bus_file:
+        return json.loads(bus_file.read())
+
 
 async def echo_server(request):
     ws = await request.accept()
+
+    test_bus_info = load_bus_route(156)
+
     while True:
         try:
+            for lat, lng in cycle(test_bus_info['coordinates']):
+                test_message = {
+                    'msgType': 'Buses',
+                    'buses': [{
+                        "busId": "c790сс",
+                        'lat': lat,
+                        'lng': lng,
+                        "route": test_bus_info['name']
+                    }]}
+                await ws.send_message(json.dumps(test_message))
+                await trio.sleep(1)
 
-            message = await ws.get_message()
-            buses = [
-                {"busId": "c790сс", "lat": 55.7500,
-                 "lng": 37.600, "route": "120"},
-                {"busId": "a134aa", "lat": 55.7494,
-                    "lng": 37.621, "route": "670к"},
-            ]
-            response = json.dumps({
-                "msgType": "Buses",
-                "buses": buses
-            })
-            await ws.send_message(response)
         except ConnectionClosed:
             break
 
