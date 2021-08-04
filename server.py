5056253adb3e996c6d1d42bed52b028a37e86944
Dimk00z <dimk00z@gmail.com>
2021-08-04 10:40:07 +0300

    init srver

---
+++ server.py
@@ -0,0 +1,30 @@
+import trio
+import json
+from trio_websocket import serve_websocket, ConnectionClosed
+
+
+async def echo_server(request):
+    ws = await request.accept()
+    while True:
+        try:
+
+            message = await ws.get_message()
+            buses = [
+                {"busId": "c790сс", "lat": 55.7500,
+                 "lng": 37.600, "route": "120"},
+                {"busId": "a134aa", "lat": 55.7494,
+                    "lng": 37.621, "route": "670к"},
+            ]
+            response = json.dumps({
+                "msgType": "Buses",
+                "buses": buses
+            })
+            await ws.send_message(response)
+        except ConnectionClosed:
+            break
+
+
+async def main():
+    await serve_websocket(echo_server, '127.0.0.1', 8000, ssl_context=None)
+
+trio.run(main)
